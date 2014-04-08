ceab7817e487aa4b8f20b8c76420dbb994c9ec4f
jbott <snowleopardjb@gmail.com>
2014-04-08 18:22:50 -0400

    Stop creating two instances of the UDP Reciever service

--- HomeControl/src/main/java/us/johnott/homecontrol/util/UDPReceiver.java
+++ HomeControl/src/main/java/us/johnott/homecontrol/util/UDPReceiver.java
@@ -14,29 +14,39 @@ public class UDPReceiver extends Service {
     private static final String LOG_NAME = UDPReceiver.class.getSimpleName();
     private static final int UDP_PORT = 12001;
 
-    private UDPReceiveThread receiveThread;
-    private PowerManager pm;
-    private PowerManager.WakeLock wakeLock;
+    private static UDPReceiveThread receiveThread;
+    private static PowerManager pm;
+    private static PowerManager.WakeLock wakeLock;
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         // Remove the thread, as it is no longer needed
         receiveThread.interrupt();
+        receiveThread = null;
         wakeLock.release();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Get wakelock
-        pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, LOG_NAME);
-        wakeLock.acquire();
+        if (pm == null) {
+            pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, LOG_NAME);
+        }
+        // Acquire wakelock
+        if (!wakeLock.isHeld()) {
+            wakeLock.acquire();
+        }
 
-        // Start UDP Receive thread
-        receiveThread = new UDPReceiveThread(UDP_PORT);
-        receiveThread.start();
-        Log.i(LOG_NAME, "Started!");
+        if (receiveThread == null) {
+            // Start UDP Receive thread
+            receiveThread = new UDPReceiveThread(UDP_PORT);
+            receiveThread.start();
+            Log.i(LOG_NAME, "Started!");
+        } else {
+            Log.i(LOG_NAME, "Receiver already started!");
+        }
+
         return START_STICKY;
     }
 
