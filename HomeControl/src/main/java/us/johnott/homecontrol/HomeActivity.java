60d6a5548441e4be5ffaba41137c3cf22255e14d
jbott <snowleopardjb@gmail.com>
2014-04-08 18:25:03 -0400

    Add exit action to menubar

--- HomeControl/src/main/java/us/johnott/homecontrol/HomeActivity.java
+++ HomeControl/src/main/java/us/johnott/homecontrol/HomeActivity.java
@@ -3,6 +3,9 @@ package us.johnott.homecontrol;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import us.johnott.homecontrol.util.UDPReceiver;
@@ -20,9 +23,29 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
         View view = getWindow().getDecorView();
         view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         Intent udpReceiver = new Intent(this, UDPReceiver.class);
         this.startService(udpReceiver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_exit:
+                // Exit the application
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
