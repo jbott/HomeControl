6f84286239eba28a5ecec29abf1ee1f446db6e95
jbott <snowleopardjb@gmail.com>
2014-05-25 00:47:54 -0400

    Lower timeout threshold, and automatically recreate the socket after more than 15 seconds have passed

--- HomeControl/src/main/java/us/johnott/homecontrol/mpd/MPDConnection.java
+++ HomeControl/src/main/java/us/johnott/homecontrol/mpd/MPDConnection.java
@@ -9,14 +9,23 @@ public class MPDConnection {
 
     private static MPDSocket socket;
     private MPDCommand lastCommand;
+    private long lastSend;
 
     public MPDConnection(String ip, int port) {
         socket = new MPDSocket(ip, port);
+        lastSend = System.currentTimeMillis();
     }
 
     // This call is blocking
     public void sendCommand(MPDCommand command) {
         Log.i(LOG_NAME, "Sending command: " + command.getClass().getSimpleName());
+        if ((System.currentTimeMillis() - lastSend) > 15000) {
+            // Reopen socket if it has already most likely timed out
+            Log.i(LOG_NAME, "Over 15 seconds, reopening socket");
+            socket.closeSocket();
+            socket.openSocket();
+        }
+        lastSend = System.currentTimeMillis();
         lastCommand = command;
         command.setMpdConnection(this);
         socket.sendCommand(command.getCommand());
@@ -32,7 +41,7 @@ public class MPDConnection {
     private boolean getResponse() {
         String response = "";
         long startTime = System.currentTimeMillis();
-        while (((System.currentTimeMillis() - startTime) < 5000) &&
+        while (((System.currentTimeMillis() - startTime) < 1000) &&
                 !socket.hasError()) {
             String last;
             while (!(last = socket.getInputLine().trim()).equals("")) {
